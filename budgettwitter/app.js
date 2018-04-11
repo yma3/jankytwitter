7bf0a4e33cb32166cb955351034b5fbbbea975e1
Krishna Thiyagarajan <krisht@cooper.edu>
2018-04-11 00:33:30 -0400

    Added all

--- budgettwitter/app.js
+++ budgettwitter/app.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const express = require('express') 
 const sqlite3 = require('sqlite3').verbose() 
 var db = new sqlite3.Database('twitter.db') 
@@ -67,57 +68,6 @@ app.post('/user', function(req, res){
 		}) 
 	})
 }) 
-
-
-// function insertPostTable(postID, username, postData, postTime, likeNum){
-// 	db.run("INSERT INTO postTable(postID, username, postData, postTime, likeNum) VALUES(?,?,?,?,?)", [postID, username, postData, postTime, likeNum], function(err){
-// 		if (err) {
-// 			return console.log(err.message) 
-// 		}
-// 		console.log('A row to post table') 
-// 	})
-// }
-
-// function selectFromPost(req, res){
-
-// 	db.get("SELECT * FROM userTable WHERE username=" + username + "", function(err, row){
-// 		if(err){
-// 			res.json(err) 
-// 		} 
-// 		console.log(row) 
-// 		res.json(row)
-// 	}) 
-
-// }
-
-
-// app.get('/', function(req, res){
-// 	selectFromPost(req, res)
-// }) 
-
-
-
-// app.post('/user', function(req, res){
-// 	username = req.query['username']
-// 	password = req.query['password']
-// 	firstName = null //req.query['firstName']
-// 	lastName = null //req.query['lastName']
-// 	email = null //req.query['email']
-// 	dob = null //new Date(req.query['dob'])
-// 	numFollow = 0 //0 
-
-// 	insertUserTable(username, password, firstName, lastName, email, dob, numFollow)
-
-// 	// db.get("SELECT * FROM userTable WHERE username=" + username + "", function(err, row){
-// 	// 	if(err){
-// 	// 		res.json(err) 
-// 	// 	} 
-// 	// 	console.log(row) 
-// 	// 	res.json(row)
-// 	// }) 
 	
-// })
-
-	
-app.listen(3000, () => console.log('Example app listening on port 3000!')) 
+app.listen(3000, 'localhost',  () => console.log('Example app listening on port 3000!')) 
 
